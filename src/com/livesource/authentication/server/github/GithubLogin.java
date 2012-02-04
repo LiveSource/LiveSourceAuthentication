8a2d1458d5f85be244c0b35bf69ddddf2b5a29be
Owner <Owner@Toshiba>
2012-02-04 15:25:48 -0800

    logout

--- src/com/livesource/authentication/server/github/GithubLogin.java
+++ src/com/livesource/authentication/server/github/GithubLogin.java
@@ -4,19 +4,20 @@ import com.livesource.authentication.server.URLUtil;
 
 public class GithubLogin {
 
-	private static final String SECRET_github = "8e8ae6b7cb786bc8734f3e493d403ae7f3e6d5c0"; // Testing
-	private static final String APPLICATION_ID_github = "51ae1237beba4135e83f"; // Testing
+//	private static final String SECRET_github = "8e8ae6b7cb786bc8734f3e493d403ae7f3e6d5c0"; // Testing
+	//private static final String APPLICATION_ID_github = "51ae1237beba4135e83f"; // Testing
 
-	//private static final String SECRET_github = "e8da41753e1e443e28064e8835de3a89a53b9945";
-	//private static final String APPLICATION_ID_github = "0fc5843521cee3e10e06";
+	private static final String SECRET_github = "e8da41753e1e443e28064e8835de3a89a53b9945";
+	private static final String APPLICATION_ID_github = "0fc5843521cee3e10e06";
 
 	private static final String GITHUB_URL = "https://github.com/login/oauth/authorize";
 	private static final String github_OAUTH_URL = "https://github.com/login/oauth/access_token";
 
-	private static final String REDIRECT_URL = "http://127.0.0.1:8888/LiveSourceApp.html?gwt.codesvr=127.0.0.1:9997"; // Testing
+	//private static final String REDIRECT_URL = "http://127.0.0.1:8888/LiveSourceApp.html?gwt.codesvr=127.0.0.1:9997"; // Testing
 	//private static final String REDIRECT_URL = "http://127.0.0.1:8888/ProjectnectionWeb.html?gwt.codesvr=127.0.0.1:9997"; // Testing
 	//private static final String REDIRECT_URL = "http://127.0.0.1:8888/LiveSourceAuthentication.html?gwt.codesvr=127.0.0.1:9997"; // Testing
 	//private static final String REDIRECT_URL = "http://www.golivesource.com";
+	private static final String REDIRECT_URL = "http://livesourceapp.appspot.com";	
 
 	public static String login(final String authenticationCode) { 
 
